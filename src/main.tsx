750c1c6b544c36b9736fa89b6d57e55ae2acac9b
UDM2202 <ulokajidaniel@gmail.com>
2026-09-24 14:05:55 +0100

    Update farcaster

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import { Buffer } from 'buffer'
-window.Buffer = window.Buffer || Buffer
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import '@solana/wallet-adapter-react-ui/styles.css'
 import './index.css'
 import App from './App'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
